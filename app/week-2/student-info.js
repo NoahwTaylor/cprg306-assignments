9c9c61eb1c249953aa0db362851de803ef126d7a
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-09-15 15:45:21 -0600

    Creates StudentInfo component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+  return (
+    <div>
+      <h1>Name: Noah Taylor</h1>
+      <p>
+        Github:{" "} 
+        <Link href="https://github.com/NoahwTaylor/cprg306-assignments">
+          My Repository
+        </Link>
+
+      </p>
+    </div>
+  );
+}
