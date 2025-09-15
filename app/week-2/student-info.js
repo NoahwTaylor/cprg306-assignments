1eeac31faca3d0d6e69b61dc2a2708ce8e2aa907
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-09-15 15:47:35 -0600

    Fixes minor formatting issue

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -9,7 +9,6 @@ export default function StudentInfo(){
         <Link href="https://github.com/NoahwTaylor/cprg306-assignments">
           My Repository
         </Link>
-
       </p>
     </div>
   );
