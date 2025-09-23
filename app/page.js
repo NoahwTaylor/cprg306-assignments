ee212a3162b82aca90e9c4259c21bb6a9baed00b
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-09-22 23:48:14 -0600

    Adds link to week-3 page

--- app/page.js
+++ app/page.js
@@ -5,8 +5,11 @@ export default function Home() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="week-2">
+      <Link href="week-2" className="mr-2">
         Go to Week 2
+      </Link>
+      <Link href="week-3" className="mr-2">
+        → Go to Week 3 
       </Link>
     </div>
   );
