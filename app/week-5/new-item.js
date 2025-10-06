819f347322ad466fcb73b29b6ac9e81c2e3f56dd
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-10-06 15:26:09 -0600

    Refactors increment/decrement functions to be inline

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -3,23 +3,7 @@ import { useState } from "react";
 
 export default function NewItem()
 {
-  const [quantity, setQuantity] = useState(0);
-
-  function Increment()
-  {
-    if(quantity < 20)
-      {
-        setQuantity(quantity + 1);
-      }
-  }
-
-  function Decrement()
-  {
-    if(quantity > 1)
-      {
-        setQuantity(quantity - 1)
-      }  
-  }
+  const [quantity, setQuantity] = useState(1);
 
   return(
     
@@ -29,8 +13,8 @@ export default function NewItem()
       <div className="flex flex-col justify-center bg-white h-36 w-1/5 rounded-sm p-5">
         <p className="text-black">Quantity: <b>{quantity}</b></p>
         <div className="">
-          <button className="w-10 h-10 bg-gray-200 hover:bg-gray-300 rounded-md mr-1.5 text-black shadow-md" onClick={Decrement}>-</button>
-          <button className="w-10 h-10 bg-blue-500 hover:bg-blue-600 rounded-md shadow-md" onClick={Increment}>+</button>
+          <button className="w-10 h-10 bg-gray-200 hover:bg-gray-300 rounded-md mr-1.5 text-black shadow-md" onClick={() => {if(quantity > 1){setQuantity(quantity-1)}}}>-</button>
+          <button className="w-10 h-10 bg-blue-500 hover:bg-blue-600 rounded-md shadow-md" onClick={() => {if(quantity < 20){setQuantity(quantity + 1)}}}>+</button>
         </div> 
         <p className="text-black text-xs">Allowed Range: 1-20</p>
       </div>
