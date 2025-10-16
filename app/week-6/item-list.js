bae2921b1c807c0fd94c865821541a63c5444d06
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-10-16 01:52:05 -0600

    Changes button styling

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -8,8 +8,6 @@ export default function ItemList(){
   const [sortBy, setSortby] = useState("name")
   const [items, setItems] = useState([...data])
 
-  
-
   const sortingChoice = (sortBy) => {
     if(sortBy === "name")
     {
@@ -32,8 +30,8 @@ export default function ItemList(){
           <h1>Shopping List</h1>
           <div className="font-extralight">
             <p className="text-xs my-3">Sort By:</p>
-            <button name="name-sort" className=" w-19 p-2 h-10 bg-gray-200 hover:bg-gray-300 rounded-md mr-1.5 text-black shadow-md text-sm" onClick={() => setSortby("name")}>Name</button>
-            <button name="category-sort" className="w-19 p-2 h-10 bg-blue-500 hover:bg-blue-600 rounded-md text-white shadow-md text-sm" onClick={() => setSortby("category")}>Category</button>
+            <button name="name-sort" className=" w-1/5 p-2 h-10 bg-gray-200 hover:bg-gray-300 rounded-md mr-1.5 text-black shadow-md text-sm" onClick={() => setSortby("name")}>Name</button>
+            <button name="category-sort" className="w-1/5 p-2 h-10 bg-blue-500 hover:bg-blue-600 rounded-md text-white shadow-md text-sm" onClick={() => setSortby("category")}>Category</button>
           </div>
         </header>
         {items.map((item, index) => (
