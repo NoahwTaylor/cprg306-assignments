33bb0b849e38846ed4a52b230628f744e17f685a
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-11-10 21:57:59 -0700

    Adds authentication functionality

---
+++ app/week-9/page.js
@@ -0,0 +1,59 @@
+"use client"
+import Link from "next/link";
+import { useUserAuth } from "../../contexts/AuthContext";
+
+
+export default function page(){
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  async function handleSignIn()
+  {
+    try
+    {
+      await gitHubSignIn();
+    }
+    catch(error)
+    {
+      console.log(error);
+    }
+  }
+
+  async function handleSignOut()
+  {
+    try
+    {
+      await firebaseSignOut();
+    }
+    catch(error)
+    {
+      console.log(error);
+    }
+  }
+
+  if (user === null)
+  {
+    return(
+      <div>
+        <div className="flex flex-col items-center space-y-2">
+          <p className="text-8xl mt-20 font-extrabold">WELCOME!</p>
+          <p className="text-4xl mt-20">PLEASE LOG-IN:</p>
+          <button className="px-4 bg-blue-500 rounded-md w-md h-20 mt-20 font-bold"onClick={handleSignIn}>GITHUB LOG-IN</button>
+        </div>
+      </div>
+    ) 
+  }
+  else
+  {
+    return(
+      <div>
+        <div className="flex flex-col items-center space-y-2">
+          <p className="text-8xl mt-20 font-extrabold">WELCOME!</p>
+          <Link href="/week-9/shopping-list">
+            <button className="px-4 bg-green-400 rounded-md w-md h-20 mt-20 font-bold">CONTINUE TO APP</button>
+          </Link>  
+          <button className="px-4 bg-blue-500 rounded-md w-md h-20 mt-5 font-bold"onClick={handleSignOut}>LOGOUT</button>
+        </div>
+      </div>
+    )
+  }
+}
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -4,25 +4,29 @@ import ItemList from "./item-list"
 import NewItem from "./new-item"; 
 import itemData from "./items.json";
 import MealIdeas from "./meal-ideas";
-
-
+import { useUserAuth } from "../../../contexts/AuthContext";
 
 export default function Page() {
+
+  const { user } = useUserAuth();
+
   const [items, setItems] = useState([...itemData])
   const [selectedItemName, setSelectedItemName] = useState();
 
   const handleAddItem = (newItem) => {setItems((prevItems) => [...prevItems, newItem])}
   const handleItemSelect = (name) => {setSelectedItemName(name)}
 
-  return(
-    <div className="flex flex-crow items-start justify-center gap-6 p-6">
-      <div>
-        <NewItem onAddItem={handleAddItem}/>
-        <ItemList items={items} onItemSelect={handleItemSelect}/>
-      </div>
+  if (user !== null)
+  {
+    return(
+      <div className="flex flex-crow items-start justify-center gap-6 p-6">
+        <div>
+          <NewItem onAddItem={handleAddItem}/>
+          <ItemList items={items} onItemSelect={handleItemSelect}/>
+        </div>
 
-      <MealIdeas ingredient={selectedItemName}/>
-    </div>
-    
-  );
+        <MealIdeas ingredient={selectedItemName}/>
+      </div> 
+    );
+  }  
 }
